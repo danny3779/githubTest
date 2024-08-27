9f3658d99d80e13408361d821a2475df3206b25e
great3779 <great3779@sina.com>
2024-08-27 22:27:58 +0800

    update.

--- libhvDemo/libhvDemo/libhvClient/libhvClient.cpp
+++ libhvDemo/libhvDemo/libhvClient/libhvClient.cpp
@@ -2,6 +2,7 @@
 #include "hv/htime.h"
 #include <string>
 #include <thread>
+#include <iostream>
 using namespace hv;
 using namespace std;
 
@@ -24,34 +25,34 @@ int main(int argc, char* argv[])
     {
       printf("connected to %s! connfd=%d\n", peeraddr.c_str(), channel->fd());
       // send(time) every 3s
-      setInterval(3000, [channel](TimerID timerID)
-      {
-        if (channel->isConnected()) 
-        {
-          char str[DATETIME_FMT_BUFLEN] = { 0 };
-          datetime_t dt = datetime_now();
-          datetime_fmt(&dt, str);
+      //setInterval(3000, [channel](TimerID timerID)
+      //{
+      //  //if (channel->isConnected()) 
+      //  //{
+      //  //  char str[DATETIME_FMT_BUFLEN] = { 0 };
+      //  //  datetime_t dt = datetime_now();
+      //  //  datetime_fmt(&dt, str);
 
-          string sss = "33";
+      //  //  string sss = "33";
 
-          unpack_setting_t unpackSetting;
-          memset(&unpackSetting, 0, sizeof(unpack_setting_t));
-          unpackSetting.mode = UNPACK_BY_LENGTH_FIELD;
-          unpackSetting.package_max_length = DEFAULT_PACKAGE_MAX_LENGTH;
-          unpackSetting.body_offset = 5;
-          unpackSetting.length_field_offset = 1;
-          unpackSetting.length_field_bytes = 4;
-          unpackSetting.length_field_coding = ENCODE_BY_BIG_ENDIAN;
-          unpackSetting.length_adjustment = 0;
-          channel->setUnpack(&unpackSetting);
+      //  //  unpack_setting_t unpackSetting;
+      //  //  memset(&unpackSetting, 0, sizeof(unpack_setting_t));
+      //  //  unpackSetting.mode = UNPACK_BY_LENGTH_FIELD;
+      //  //  unpackSetting.package_max_length = DEFAULT_PACKAGE_MAX_LENGTH;
+      //  //  unpackSetting.body_offset = 5;
+      //  //  unpackSetting.length_field_offset = 1;
+      //  //  unpackSetting.length_field_bytes = 4;
+      //  //  unpackSetting.length_field_coding = ENCODE_BY_BIG_ENDIAN;
+      //  //  unpackSetting.length_adjustment = 0;
+      //  //  channel->setUnpack(&unpackSetting);
 
-          //channel->write(sss);
-        }
-        else 
-        {
-          killTimer(timerID);
-        }
-      });
+      //  //  //channel->write(sss);
+      //  //}
+      //  //else 
+      //  //{
+      //  //  killTimer(timerID);
+      //  //}
+      //});
     }
     else
     {
@@ -60,7 +61,24 @@ int main(int argc, char* argv[])
   };
   cli.onMessage = [](const SocketChannelPtr& channel, Buffer* buf)
   {
-    printf("< %.*s\n", (int)buf->size(), (char*)buf->data());
+    auto pdata = (char*)buf->data();
+    auto pid = channel->fd();
+
+    auto size = (int)buf->size();
+
+    // 数据包长度tLen
+    {
+      string strLen((char*)buf->data() + 1, 4);
+      long len = 0;
+      memcpy(&len, strLen.c_str(), 4);
+      //auto len = atol(strLen.c_str());
+      auto tLen = ntohl(len);
+    }
+
+    string msg((char*)buf->data() + 5, (int)buf->size() - 5);
+    std::cout << msg << std::endl;
+
+    //printf("< %.*s\n", (int)buf->size(), (char*)buf->data()+5);
   };
   cli.onWriteComplete = [](const SocketChannelPtr& channel, Buffer* buf)
   {
@@ -76,25 +94,19 @@ int main(int argc, char* argv[])
 
   unpack_setting_t unpackSetting;
   memset(&unpackSetting, 0, sizeof(unpack_setting_t));
-  unpackSetting.mode = UNPACK_BY_LENGTH_FIELD;
-  unpackSetting.package_max_length = DEFAULT_PACKAGE_MAX_LENGTH;
-  unpackSetting.body_offset = 5;
-  unpackSetting.length_field_offset = 1;
-  unpackSetting.length_field_bytes = 4;
-  unpackSetting.length_field_coding = ENCODE_BY_BIG_ENDIAN;
-  unpackSetting.length_adjustment = 0;
+  unpackSetting.mode = UNPACK_BY_LENGTH_FIELD; // 设置解包模式：按照字段长度解包
+  unpackSetting.package_max_length = DEFAULT_PACKAGE_MAX_LENGTH; // 设置整包最大长度，默认2M
+  unpackSetting.body_offset = 5; // 设置包体偏移量
+  unpackSetting.length_field_offset = 1; // 设置长度偏移量
+  unpackSetting.length_field_bytes = 4; // 设置长度字节数
+  unpackSetting.length_adjustment = 0; // 设置长度调整
+  unpackSetting.length_field_coding = ENCODE_BY_BIG_ENDIAN; // 设置字节编码规则，默认"大字节"序
   cli.setUnpack(&unpackSetting);
 
   cli.start();
 
   std::thread t([&]()
   {
-    while (true)
-    {
-      Sleep(3000);
-      string str("33");
-      cli.send(str);
-    }
   });
 
   // press Enter to stop
--- libhvDemo/libhvDemo/libhvDemo/libhvServer.cpp
+++ libhvDemo/libhvDemo/libhvDemo/libhvServer.cpp
@@ -1,12 +1,16 @@
 ﻿#include "hv/TcpServer.h"
 using namespace hv;
 
+
+
 #include <sstream>
+#include <iostream>
 using namespace std;
 
 #pragma comment(lib, "hv.lib")
 
 std::map<int, SocketChannelPtr> gDictChannel;
+std::mutex mtx;
 
 int main(int argc, char* argv[])
 {
@@ -21,8 +25,11 @@ int main(int argc, char* argv[])
   printf("server listen on port %d, listenfd=%d ...\n", port, listenfd);
   srv.onConnection = [](const SocketChannelPtr& channel)
   {
+    mtx.lock();
+    std::lock_guard<std::mutex> lck(mtx, std::adopt_lock);
+
     std::string peeraddr = channel->peeraddr();
-    if (channel->isConnected()) 
+    if (channel->isConnected())
     {
       printf("%s connected! connfd=%d\n", peeraddr.c_str(), channel->fd());
       gDictChannel[channel->fd()] = channel;
@@ -32,6 +39,12 @@ int main(int argc, char* argv[])
       printf("%s disconnected! connfd=%d\n", peeraddr.c_str(), channel->fd());
       gDictChannel.erase(channel->fd());
     }
+
+    // //启动定时器
+    //setInterval(3000, [=](TimerID timerID)
+    //{
+    //  channel->close();
+    //});
   };
   srv.onMessage = [](const SocketChannelPtr& channel, Buffer* buf)
   {
@@ -41,42 +54,60 @@ int main(int argc, char* argv[])
   };
   srv.onWriteComplete = [](const SocketChannelPtr& channel, Buffer* buf)
   {
+    string msg((char*)buf->data(), (int)buf->size());
+    stringstream ss;
+    ss << channel->fd();
+    msg = ss.str() + msg;
     //printf("> %.*s\n", (int)buf->size(), (char*)buf->data());
+    cout << msg << endl;
   };
 
   unpack_setting_t unpackSetting;
   memset(&unpackSetting, 0, sizeof(unpack_setting_t));
   unpackSetting.mode = UNPACK_BY_LENGTH_FIELD; // 设置解包模式：按照字段长度解包
   unpackSetting.package_max_length = DEFAULT_PACKAGE_MAX_LENGTH; // 设置整包最大长度，默认2M
-
-
   unpackSetting.body_offset = 5; // 设置包体偏移量
   unpackSetting.length_field_offset = 1; // 设置长度偏移量
   unpackSetting.length_field_bytes = 4; // 设置长度字节数
   unpackSetting.length_adjustment = 0; // 设置长度调整
-
-
   unpackSetting.length_field_coding = ENCODE_BY_BIG_ENDIAN; // 设置字节编码规则，默认"大字节"序
   srv.setUnpack(&unpackSetting);
 
   srv.setThreadNum(4);
+  srv.setLoadBalance(LB_LeastConnections);
   srv.start();
 
   std::thread t([&]()
   {
+    int counter = 0;
     while (true)
     {
-      int counter = 0;
       std::stringstream ss;
-      ss << counter++;
-      for (auto it = gDictChannel.begin(); it != gDictChannel.end(); ++it)
+      ss << counter++ << "..sdfsdwerewrewrewr444444!!!!!";
+      auto msg = ss.str();
+
+      char headerPrefix = 'x';
+
+      std::vector<char> buf_new(msg.size() + 5);
+      auto len_2 = htonl(msg.size());
+      memcpy(&buf_new[0], &headerPrefix, sizeof(char));
+      memcpy(&buf_new[0] + 1, &len_2, sizeof(len_2));
+      memcpy(&buf_new[0] + 5, msg.c_str(), msg.size());
       {
-        //it->second->write(ss.str());
+        mtx.lock();
+        std::lock_guard<std::mutex> lck(mtx, std::adopt_lock);
+        for (auto it = gDictChannel.begin(); it != gDictChannel.end(); ++it)
+        {
+          // 向某个客户端发送数据
+          //it->second->write(ss.str());
+        }
       }
 
-      srv.broadcast(ss.str());
+      // 向全部客户端发送数据
+      srv.broadcast(&buf_new[0], buf_new.size());
 
-      std::this_thread::sleep_for(std::chrono::seconds(3));
+      std::this_thread::sleep_for(std::chrono::seconds(1));
+      //std::this_thread::sleep_for(std::chrono::milliseconds(10));
     }
   });
 
